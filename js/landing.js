dc5bb19508a7efa23dd19d10ac741b1ba9cf7a08
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-12-24 16:15:16 +0530

    landing page,footer
    s10
    s11 alignments
    s12 alignments
    footer alignments

--- js/landing.js
+++ js/landing.js
@@ -11,17 +11,33 @@ document.addEventListener("DOMContentLoaded", () => {
 const buildLandingPageS3SwiperInitiator = () => {
   if (window.innerWidth > 1400) {
     buildLandingPageS3Swiper(4, 30);
+    buildLandingPageS10Swiper(1,30);
   } else if (window.innerWidth <= 1400 && window.innerWidth > 998) {
     buildLandingPageS3Swiper(3, 20);
+    buildLandingPageS10Swiper(1,30);
   } else if (window.innerWidth <= 998 && window.innerWidth > 600) {
     buildLandingPageS3Swiper(2, 10);
+    buildLandingPageS10Swiper(1,30);
   } else if (window.innerWidth <= 600) {
     buildLandingPageS3Swiper(1, 5);
+    buildLandingPageS10Swiper(1,30);
   }
 };
 
 const buildLandingPageS3Swiper = (perView, space) => {
   const swiper = new Swiper(".lpLatestProductSwiper", {
+    slidesPerView: perView,
+    spaceBetween: space,
+    navigation: {
+      nextEl: ".lp-s3-swiper-control-right",
+      prevEl: ".lp-s3-swiper-control-left",
+    },
+  });
+};
+
+
+const buildLandingPageS10Swiper = (perView, space) => {
+  const swiper = new Swiper(".lptestamonialSwiper", {
     slidesPerView: perView,
     spaceBetween: space,
     navigation: {
