5c08204ebfb6dd4d0131d43f5aab6acb624c7f60
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-01 16:15:17 +0530

    minor update

--- interface/components/newsletter.php
+++ interface/components/newsletter.php
@@ -5,7 +5,7 @@
             <h5 class="cey-text-white pb-2">SUBSCRIBE TO OUR NEW LETTER</h5>
             <div class="newsletter-input-box gap-0 ">
                 <input class="newsletter-textbox border-0" type="text" placeholder="ENTER YOUR EMAIL">
-                <button class="newsletter-button p-lg-3  p-md-3 p-2 cey-bg-primary fw-bold border-0">SUBSCRIBE</button>
+                <button class="newsletter-button p-lg-3  p-md-3 p-1 cey-bg-primary fw-bold border-0">SUBSCRIBE</button>
             </div>
         </div>
     </div>
--- interface/components/visionmission.php
+++ interface/components/visionmission.php
@@ -1,13 +1,13 @@
 <!-- vision mission section -->
-<section class="vision-mission cey-bg-darker p-5">
+<section class="vision-mission cey-bg-darker p-lg-5 p-1 py-3">
     <div class="h-100 d-flex flex-column flex-lg-row container text-center">
-        <div class="p-5 cey-bg-dark col-12 col-lg-6 vision-mission-vision d-flex flex-column gap-2 cey-text-white">
+        <div class="p-5 cey-bg-dark col-12 col-lg-6 vision-mission-vision d-flex flex-column gap-3 cey-text-white">
             <h3 class="cey-main-font">VISION</h3>
-            <p class="fs-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni suscipit praesentium animi id! Omnis cumque nesciunt reprehenderit eaque repellendus cum porro quos consequuntur reiciendis a earum corrupti, exercitationem, esse qui!</p>
+            <p class="fs-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni suscipit praesentium animi id! Omnis cumque nesciunt reprehenderit eaque repellendus cum porro quos consequuntur reiciendis a earum corrupti, exercitationem, esse qui!</p>
         </div>
-        <div class="p-5 cey-bg-white col-12 col-lg-6 vision-mission-mission d-flex flex-column gap-2">
+        <div class="p-5 cey-bg-white col-12 col-lg-6 vision-mission-mission d-flex flex-column gap-3">
             <h3 class="cey-main-font">MISSION</h3>
-            <p class="fs-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni suscipit praesentium animi id! Omnis cumque nesciunt reprehenderit eaque repellendus cum porro quos consequuntur reiciendis a earum corrupti, exercitationem, esse qui!</p>
+            <p class="fs-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni suscipit praesentium animi id! Omnis cumque nesciunt reprehenderit eaque repellendus cum porro quos consequuntur reiciendis a earum corrupti, exercitationem, esse qui!</p>
         </div>
     </div>
 </section>
