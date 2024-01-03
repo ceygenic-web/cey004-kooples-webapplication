63894a28178e06cd0782b322b9a1c3caac2e6900
Shenal Rayantha <101634665+shenal-rd203@users.noreply.github.com>
2024-01-03 14:31:07 +0530

    created productAdd.php and productUpdate.php and changed the db password

---
+++ interface/admin/pages/productAdd.php
@@ -0,0 +1,14 @@
+<section class="productAdd ad-s1 cey-bg-dark py-5">
+    <div class="container py-5 d-flex justify-content-center align-items-center h-100 w-100 flex-column">
+        <h1 class="cey-text-white">Add products</h1>
+        <form id="productAdd" class="col-12 col-lg-6 col-md-8 d-flex flex-column gap-1 cey-text-white" method="POST" action="../../backend/api/product.php">
+            <label for="title">Title</label>
+            <input type="text" id="title" name="title" class="form-control"><br>
+            <label for="description">Description</label>
+            <input type="text" id="description" name="description"><br>
+            <label for="category">Category</label>
+            <input type="text" id="category" name="category"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</section>
