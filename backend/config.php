08cd3499ac1e5a41d187f6cb01bd5e82d2bb5cdc
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-06 21:57:44 +0530

    db updated

--- backend/config.php
+++ backend/config.php
@@ -5,7 +5,7 @@ define("SESSION_VARIABLE_USER", "cey004_user");
 
 // database
 define("DB_SERVER", "localhost");
-define("DB_USERNAME", "kooplesc_kooples	");
+define("DB_USERNAME", "kooplesc_kooples");
 define("DB_PASSWORD", "SwFMjz5a1VCe");
 // define("DB_PASSWORD", "");
-define("DB_DATABASE", "kooplesc_kooples	");
+define("DB_DATABASE", "kooplesc_kooples");
