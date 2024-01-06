755ef79fde60bc776a9376958cf12cf5c9491786
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-06 20:58:25 +0530

    config updated

--- backend/config.php
+++ backend/config.php
@@ -1,5 +1,5 @@
 <?php
-define("ROOT", "http://kooplesclothing.com/");
+define("ROOT", "https://kooplesclothing.com/");
 define("SESSION_VARIABLE_ADMIN", "cey004_admin");
 define("SESSION_VARIABLE_USER", "cey004_user");
 
