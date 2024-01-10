0ba4f6b007574565ee6f53b56c7899ddbbb0d3dc
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-11 00:19:56 +0530

    UI updated and server connected

--- interface/admin/components/productUpdate.php
+++ interface/admin/components/productUpdate.php
@@ -1,5 +1,5 @@
 <!-- product update section -->
-<section class="productUpdate ad-s1 cey-bg-dark py-5">
+<section class="d-none productUpdate ad-s1 cey-bg-dark py-5">
     <div class="container py-5 d-flex justify-content-center align-items-center h-100 w-100 flex-column">
         <h1 class="cey-text-white">Update products</h1>
         <form id="productUpdate" class="col-12 col-lg-6 col-md-8 d-flex flex-column gap-1 cey-text-white">
