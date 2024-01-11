a31b5f8ba657c0ba9f31b0ca89058e436a5656ae
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-12 03:30:06 +0530

    updated

--- backend/config.php
+++ backend/config.php
@@ -1,11 +1,11 @@
 <?php
-define("ROOT", "https://kooplesclothing.com/");
+define("ROOT", "http://localhost:9001/");
 define("SESSION_VARIABLE_ADMIN", "cey004_admin");
 define("SESSION_VARIABLE_USER", "cey004_user");
 
 // database
 define("DB_SERVER", "localhost");
-define("DB_USERNAME", "kooplesc_kooples");
-define("DB_PASSWORD", "SwFMjz5a1VCe");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "JanithNirmal12#$");
 // define("DB_PASSWORD", "");
-define("DB_DATABASE", "kooplesc_kooples");
+define("DB_DATABASE", "cey004_db");
