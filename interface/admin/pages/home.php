31fe9e8e7dfd78f3373e2038d45843a35a228d54
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-12 04:29:53 +0530

    css updated

--- interface/admin/pages/home.php
+++ interface/admin/pages/home.php
@@ -1,7 +1,7 @@
 <!-- admin home page -->
 <!-- section 1 - home main -->
 <section class="ahp-s1 cey-bg-dark py-2">
-    <div class="container d-flex cey-text-white flex-wrap py-3 cey-bg-darker h-100">
+    <div class="container d-flex cey-text-white flex-wrap py-3 cey-bg-darker">
         <div class="p-3 col-12 col-lg-4 border-secondary border-1 border-end d-flex flex-column gap-3">
             <button data-panel="product" class="admin-navigation-button cey-btn-box w-100">Products</button>
             <button data-panel="blog" class="admin-navigation-button cey-btn-box w-100">Blog</button>
