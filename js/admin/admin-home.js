cdbbbec7b7fc75714ce5f2e2469b7120cf816182
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-11 21:43:24 +0530

    test

--- js/admin/admin-home.js
+++ js/admin/admin-home.js
@@ -19,6 +19,8 @@ function loadPanels() {
       (json) => {
         let container = document.getElementById("product");
         let data = JSON.parse(json);
+        console.log(json);
+        console.log(data);
         if (data.status == "success") {
           container.innerHTML += data.results;
 
