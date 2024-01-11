a6e687c6c2d7ccad503a320be54a592ee195a0ed
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-11 20:51:29 +0530

    test

--- backend/router/Controller.php
+++ backend/router/Controller.php
@@ -25,7 +25,7 @@ class Controller
             $sessionManger->updateSessionVariable(SESSION_VARIABLE_ADMIN);
             if (!$sessionManger->isLoggedIn()) {
                 var_dump("redirected");
-                header("Location: /admin/login");
+                // header("Location: /admin/login");
             }
         }
 
