c4ccc51b57159d487718f8f5148f333bdd165431
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-15 17:15:48 +0530

    minor updated

--- backend/api/product.php
+++ backend/api/product.php
@@ -109,7 +109,11 @@ class Product extends Api
         if (!floatval($price)) {
             return (object)["status" => "failed", "error" => "invalid price"];
         }
-        
+
+        if (count($images) === 0) {
+            return (object)["status" => "failed", "error" => "No Image Provided"];
+        }
+
         $categoryId = $this->getData("SELECT * FROM `category` WHERE `category` ='" .  $category . "' ")[0]["category_id"];
         $subCategoryId = $this->getData("SELECT * FROM `sub_categories` WHERE `sub_category` ='" .  $subCategories . "' ")[0]["sub_categories_id"];
         if ($categoryId === null) {
@@ -119,8 +123,8 @@ class Product extends Api
             return (object)["status" => "failed", "error" => "invalid Sub Category"];
         }
 
-        return (object)["status" => "success"];
         $id = mt_rand(000000, 999999);
+        return (object)["status" => "success"];
         $this->updateData("INSERT INTO `product` 
                                     (`product_id`,`category_category_id`, `title`, `description`, `price`, `sub_categories_sub_categories_id`, `other_data`) 
                                     VALUES (?,?,?,?,?,?,?)", "sisssss", array($id, $categoryId, $title, $description, $price, $subCategoryId, $other_data));
